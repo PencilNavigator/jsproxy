ed6278e6d63dbaaf061ba0e5b3396a174ea94401
PencilNavigator <85282140+PencilNavigator@users.noreply.github.com>
2022-06-19 21:57:13 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -15,7 +15,7 @@ jsproxy_config({
     'Pencil': {
       label: 'JustHost DallasColo (Tier-3)',
       lines: {
-        '193.233.204.229:8443': 1,
+        '193.233.204.229:8080': 1,
       },
     },
     'mysite': {
